d85a199d516889b120fb4ad4525fe8e945c46c7c
EvgeniyGagen <evg961@mail.ru>
2022-08-18 18:01:06 +0300

    Excersie 23 1

---
+++ Exercise23_1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+    class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            int[] A = {1, 8, 27};
+            for(int i = 0; i < A.Length; i++)
+            {
+                Console.Write("Куб от первого числа: ");
+                Console.WriteLine(Math.Pow(A[0],3));                      
+                Console.Write("Куб от второго числа: ");
+                Console.WriteLine(Math.Pow(A[1],3));
+                Console.Write("Куб от третьего числа: ");
+                Console.WriteLine(Math.Pow(A[2],3));            
+                Console.WriteLine("y/n");
+                if(Console.ReadLine() == "n") break;
+            }
+        }
+    }
