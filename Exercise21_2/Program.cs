2f1735c5c8b0a92d1ca62da42cc6168fbd2a42ab
EvgeniyGagen <evg961@mail.ru>
2022-08-18 17:35:39 +0300

    Exercise 21 2

---
+++ Exercise21_2/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+class Program
+{
+static void Main(string[] args)
+    {
+       int n = 3;
+       int [] A = new int[n];
+       int i = 0;
+       Console.WriteLine("Введите точку А");
+       while(i < n)
+       {
+            A[i] = Convert.ToInt32(Console.ReadLine()); i++;
+       }
+        int [] B = new int[n];
+        int j = 0;
+        Console.WriteLine("Введите точку B");
+       while(j < n)
+       {
+            B[j] = Convert.ToInt32(Console.ReadLine()); j++;
+       }
+                        
+        
+        Console.Write($"Расстояние между  точками A и В  равно: ");
+        Console.WriteLine(Math.Sqrt(Math.Pow(B[0] - A[0], 2) + Math.Pow(B[1] - A[1], 2) + Math.Pow(B[2] - A[2], 2)));
+        
+        
+        
+                
+            
+    }
+    
+}
