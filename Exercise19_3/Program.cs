8ff0bf2f1cf5ba316622e9fcec3ea6206536fc33
EvgeniyGagen <evg961@mail.ru>
2022-08-18 17:06:03 +0300

    Exercise 19 3

---
+++ Exercise19_3/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+    class Program
+    {
+            
+        static void Main(string[] args)
+        {
+            // && - и
+            // || - или
+            int [] numbers = {2, 3, 4, 3, 2};
+            int len = numbers.Length;
+            for(int i = 0; i < len; i++)
+            {
+            if(numbers[0] == numbers[4] && numbers[1] == numbers[3]) // false  true
+            {
+               Console.WriteLine("палиндром"); 
+            }
+            Console.WriteLine("NO");
+        }
+    }}
